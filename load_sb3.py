daf2fbfab97a83244b90c3d42a4dd8e9c1d35852
alifuatsahin <alifuatpc@gmail.com>
2023-12-05 14:25:01 +0100

    Update load_sb3.py

--- load_sb3.py
+++ load_sb3.py
@@ -58,7 +58,7 @@ from utils.file_utils import get_latest_model, load_all_results
 LEARNING_ALG = "SAC"
 interm_dir = "./logs/intermediate_models/"
 # path to saved models, i.e. interm_dir + '121321105810'
-log_dir = interm_dir + '112623213813'
+log_dir = interm_dir + '120523140121'
 
 # initialize env configs (render at test time)
 # check ideal conditions, as well as robustness to UNSEEN noise during training
@@ -67,7 +67,7 @@ env_config['render'] = True
 env_config['record_video'] = False
 env_config['add_noise'] = True 
 env_config['motor_control_mode'] = "CARTESIAN_PD"
-env_config['observation_space_mode'] = "LR_COURSE_OBS"
+env_config['observation_space_mode'] = "CPG_OBS"
 env_config['task_env'] = "LR_COURSE_TASK"
 # env_config['test_env'] = True
 
